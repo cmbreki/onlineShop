e4ef3c919955e35602b4b472aca3d25209d49dd4
cmbreki <cbreki@hotmail.gr>
2016-06-27 00:03:55 +0300

    Add weather controller

--- js/app.js
+++ js/app.js
@@ -29,6 +29,38 @@ app.controller("PanelController",function(){
 
 });
 
+app.controller("WeatherController",function($http){
+
+ var vm = this;
+  
+  var URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+  
+  var request = {
+    method: 'GET',
+    url: URL,
+    params: {
+       q: 'NemeaCity',
+      mode: 'json',
+      units: 'imperial',
+      cnt: '7',
+      appid: 'e5656666169da366f01834bcd1d3bdcd'
+    }
+  };
+  
+  $http(request)
+    .then(function(response) {
+     vm.data = response.data;
+   
+    }).
+    catch(function(response) {
+     vm.data = response.data;
+    
+     
+   
+  });
+
+});
+
 var gems = [
 	
 	{
